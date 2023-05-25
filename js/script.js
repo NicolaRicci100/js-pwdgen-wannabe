37b675aca2a04f0203522d3eb8b1ddbf4d932fa2
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-25 16:25:44 +0200

    aggiunti comandi javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('JS OK');
+
+
+const userName = prompt('dimmi il tuo nome', 'Nicola');
+console.log('userName');
+
+const userSurname = prompt('dimmi il tuo cognome', 'Ricci');
+console.log('userSurname');
+
+const favColor = prompt('adesso dimmi il tuo colore preferito', 'Blu');
+console.log('userColor');
+
+const password = userName + userSurname + favColor + '21';
+console.log('password');
+
+document.getElementById('password').innerText = password;
